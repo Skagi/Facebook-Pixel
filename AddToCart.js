073db6b509934825bfdccb000023f3ef2ec02c75
Skagi <asaelskagi@gmail.com>
2018-10-17 11:52:22 +0200

    Update AddToCart.js

--- AddToCart.js
+++ AddToCart.js
@@ -2,9 +2,9 @@ jQuery('.add-to-basket').click(function() {
  	var price = jQuery(this).parents('.product_wrapper').find('span.price').html();
 	price = price.replace(',','.');
      fbq('track', 'AddToCart',{
-     													currency: "PLN", 
-     													value: parseFloat(price)
-   													}
+     				currency: "PLN", 
+     				value: parseFloat(price)
+   			       }
    ); 
 });
 
@@ -14,9 +14,9 @@ jQuery('#projector_button_basket').click(function() {
  	var prices = jQuery('#projector_price_value').html();
    prices = prices.replace(',','.');
    fbq('track', 'AddToCart',{
-     													currency: "PLN", 
-     													value: parseFloat(prices)
-   													}
+     			      currency: "PLN", 
+     			      value: parseFloat(prices)
+   			    }
    );
 });
 
